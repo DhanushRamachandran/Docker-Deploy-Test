fdbe496734546cb40c1a9ac042b0ac407888028e
Dhanush Ramachandran <dhanush.ramachandran@gmail.com>
2025-11-19 17:16:23 -0600

    testing changes

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
 from app import app
 
 def test_home():
-    response = app.test_client.get("/")
+    client = app.test_client()     
+    response = client.get("/")      
+
     assert response.status_code == 200
     assert response.data == b"hello world"
-    
